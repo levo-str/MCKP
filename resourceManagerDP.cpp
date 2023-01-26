c55d91baafe27afa109ad380c94fb8e6090ad33f
levo-str <kevin-lam@hotmail.fr>
2023-01-26 19:31:27 +0100

    fix dynamic programming solving of MCKP in case there is a missing set number
    The algorithm didn't work when the set number weren't all consecutive.
    For example if item could either belong to set number 0 or set number 2 (there is no set number 1) then the algorithm didn't work.
    This commit can't be cancelled in a real situation to improve the performance but in a pure MCKP problem with a lot of random number generation for tests i keep it.

--- resourceManagerDP.cpp
+++ resourceManagerDP.cpp
@@ -37,6 +37,58 @@ ResourceManagerDP::ResourceManagerDP(int numberOfResources, std::unordered_map<i
 	//std::cout << "Initialisation complete" << endl;
 }
 
+SackComposition ResourceManagerDP::solveMckp() {
+
+	std::vector<JobConfiguration> emptyVector = {};
+	SackComposition bestComposition = SackComposition(emptyVector);
+
+	//std::cout << "Solving starting ..." << endl;
+	for (int i = 0; i < numberItems + 1; i++) {
+		for (int j = 0; j < numberResources + 1; j++) {
+			//cout << "step number i = " << i << " number j = " << j << endl;
+
+			if (i == 0 || j == 0) profitArray[i][j] = 0;
+
+			else {
+				JobConfiguration currentItem = itemsSortedByTask[i - 1];
+				int previousTask = currentItem.getTask() - 1;
+
+				//cout << "item number : " << currentItem.getId() << " weighs " << currentItem.getResources() << endl;
+				if (j >= currentItem.getResources()) {
+					int maxProfit = max(profitArray[i - 1][j], currentItem.getProfit());
+					int k = j - currentItem.getResources();
+					for (int l = 1; l < i; l++) {
+						if (currentItem.getProfit() + profitArray[l][k] > maxProfit && itemsSortedByTask[l - 1].getTask() <= previousTask ) {
+							maxProfit = currentItem.getProfit() + profitArray[l][k];
+						}
+					}
+					profitArray[i][j] = maxProfit;
+				}
+
+				else {
+					profitArray[i][j] = profitArray[i - 1][j];
+				}
+			}
+		}
+	}
+	//Now get the best composition : 
+	int i = numberItems;
+	int j = numberResources;
+	int previousTask = itemsSortedByTask[i - 1].getTask();
+	while (i >= 1 && j > 0) {
+		JobConfiguration inspectedJobConfiguration = itemsSortedByTask[i - 1];
+		if (profitArray[i - 1][j] != profitArray[i][j] && inspectedJobConfiguration.getTask() <= previousTask) {
+			previousTask = inspectedJobConfiguration.getTask() - 1;
+			bestComposition.addJobConfiguration(inspectedJobConfiguration);
+			j = j - itemsSortedByTask[i - 1].getResources();
+		}
+		i--;
+	}
+
+	//std::cout << endl << "the algorithm predict an optimal profit of : " << profitArray[numberItems][numberResources] << endl;
+	//std::cout << "The profit of best composition found by the algorithm is as expected the same : " << bestComposition.calculateTotalProfit();
+	return bestComposition;
+}
 
 /**
 * Solve the MCKP according to parameters of the object Resource Manager
@@ -62,7 +114,7 @@ SackComposition ResourceManagerDP::solveMckpConcurrently() {
 					int maxProfit = max(profitArray[i - 1][j], currentItem.getProfit());
 					int k = j - currentItem.getResources();
 					for (int l = 1; l < i; l++) {
-						if (currentItem.getProfit() + profitArray[l][k] > maxProfit && itemsSortedByTask[l - 1].getTask() == previousTask) {
+						if (currentItem.getProfit() + profitArray[l][k] > maxProfit && itemsSortedByTask[l - 1].getTask() <= previousTask) {
 							maxProfit = currentItem.getProfit() + profitArray[l][k];
 						}
 					}
@@ -74,59 +126,6 @@ SackComposition ResourceManagerDP::solveMckpConcurrently() {
 				}
 			}
 			});
-	}
-	//Now get the best composition : 
-	int i = numberItems;
-	int j = numberResources;
-	int previousTask = itemsSortedByTask[i - 1].getTask();
-	while (i >= 1 && j > 0) {
-		JobConfiguration inspectedJobConfiguration = itemsSortedByTask[i - 1];
-		if (profitArray[i - 1][j] != profitArray[i][j] && inspectedJobConfiguration.getTask() <= previousTask) {
-			previousTask = inspectedJobConfiguration.getTask() - 1;
-			bestComposition.addJobConfiguration(inspectedJobConfiguration);
-			j = j - itemsSortedByTask[i - 1].getResources();
-		}
-		i--;
-	}
-
-	//std::cout << endl << "the algorithm predict an optimal profit of : " << profitArray[numberItems][numberResources] << endl;
-	//std::cout << "The profit of best composition found by the algorithm is as expected the same : " << bestComposition.calculateTotalProfit();
-	return bestComposition;
-}
-
-SackComposition ResourceManagerDP::solveMckp() {
-
-	std::vector<JobConfiguration> emptyVector = {};
-	SackComposition bestComposition = SackComposition(emptyVector);
-
-	//std::cout << "Solving starting ..." << endl;
-	for (int i = 0; i < numberItems + 1; i++) {
-		for (int j = 0; j < numberResources + 1; j++) {
-			//cout << "step number i = " << i << " number j = " << j << endl;
-
-			if (i == 0 || j == 0) profitArray[i][j] = 0;
-
-			else {
-				JobConfiguration currentItem = itemsSortedByTask[i - 1];
-				int previousTask = itemsSortedByTask[i - 1].getTask() - 1;
-
-				//cout << "item number : " << currentItem.getId() << " weighs " << currentItem.getResources() << endl;
-				if (j >= currentItem.getResources()) {
-					int maxProfit = max(profitArray[i - 1][j], currentItem.getProfit());
-					int k = j - currentItem.getResources();
-					for (int l = 1; l < i; l++) {
-						if (currentItem.getProfit() + profitArray[l][k] > maxProfit && itemsSortedByTask[l - 1].getTask() == previousTask) {
-							maxProfit = currentItem.getProfit() + profitArray[l][k];
-						}
-					}
-					profitArray[i][j] = maxProfit;
-				}
-
-				else {
-					profitArray[i][j] = profitArray[i - 1][j];
-				}
-			}
-		}
 	}
 	//Now get the best composition : 
 	int i = numberItems;
