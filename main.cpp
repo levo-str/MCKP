e2909b494b9ba7245447dbed4bfb2b7ef4928ea9
levo-str <kevin-lam@hotmail.fr>
2023-01-31 13:12:29 +0100

    create vectors for graph to measure performance

--- main.cpp
+++ main.cpp
@@ -82,95 +82,124 @@ int main() {
 
 	
 	std::vector<int> v(3);
-
+	int maxCapacity = 1000;
+	int maxNumberOfItems = 500;
+	
 	// Set up our random number generator
 	std::mt19937 rng;
 	rng.seed(std::random_device()());
-	std::uniform_int_distribution<int> dist1(1, 100);
-	std::uniform_int_distribution<int> dist2(1, 10);
-	std::uniform_int_distribution<int> dist3(1, 3);
+	std::uniform_int_distribution<int> dist1(maxCapacity / 3, maxCapacity);
+	std::uniform_int_distribution<int> dist3(1, 10);
 	//specify the number of nodes
 	//specify the number of jobs running
 	//calculate memory consumption
 	//calculate speedup of the algorithm
 	//look into other parallelization methods
-
-	long long totalTimeForSerialResolution = 0;
-	long long totalTimeForParallelResolution = 0;
-	long long totalTimeForBBResolution = 0;
-
-	for (int testNumber = 0; testNumber < 100; testNumber++) {
-
-		int numberOfSet = dist3(rng);
-		int weight = 100;
-
-		unordered_map<int, JobConfiguration> mapItems;
-
-		for (int itemNumber = 0; itemNumber < 100; itemNumber++) {
-			std::generate(begin(v), end(v), [&]() { return dist2(rng); });
-			JobConfiguration newJobConfig = JobConfiguration(v[0], v[1], v[2] % numberOfSet);
-			mapItems[newJobConfig.getId()] = newJobConfig;
-		}
-
-		auto start = get_time();
-		ResourceManagerDP problemSolver = ResourceManagerDP(weight, mapItems);
-		SackComposition solution = problemSolver.solveMckp();
-		auto finish = get_time();
-		auto duration =
-		std::chrono::duration_cast<std::chrono::milliseconds>(finish - start);
-		totalTimeForSerialResolution += duration.count();
-
-
-		auto parallelStart = get_time();
-
-		ResourceManagerDP parallelProblemSolver = ResourceManagerDP(weight, mapItems);
-		SackComposition parallelSolution = parallelProblemSolver.solveMckpConcurrently();
-
-		auto parallelFinish = get_time();
-
-		auto parallelDuration =
-			std::chrono::duration_cast<std::chrono::milliseconds>(parallelFinish - parallelStart);
-		totalTimeForParallelResolution += parallelDuration.count();
-
-		auto bBstart = get_time();
-		ResourceManagerBB problemSolverBB = ResourceManagerBB(weight, mapItems);
-		SackComposition solutionBB = problemSolverBB.solveMckp();
-
-		auto bBFinish = get_time();
-
-		auto bBDuration =
-			std::chrono::duration_cast<std::chrono::milliseconds>(bBFinish - bBstart);
-		totalTimeForBBResolution += bBDuration.count();
-
-		if (solutionBB.calculateTotalProfit() != solution.calculateTotalProfit()) {
-			std::cout << "There is a problem" << std::endl;
-			std::cout << solution.calculateTotalProfit() << endl;
-			std::cout << solutionBB.calculateTotalProfit() << endl;
-			std::cout << "knapsack :" << weight << endl;
-			std::cout << "DP Solution : " << endl;
-			for (JobConfiguration item : solution.getItemList()) {
-				cout << item.getId() << "," << item.getResources() << "," << item.getProfit() << "," << item.getTask() << endl;
-			}
-			cout << endl;
-			std::cout << "BB Solution : " << endl;
-			for (JobConfiguration item : solutionBB.getItemList()) {
-				cout << item.getId() << "," << item.getResources() << "," << item.getProfit() << "," << item.getTask() << endl;
-			}
-			cout << "mapItems" << endl;
-			for (auto [key, item] : mapItems) {
-				cout << item.getId() << "," << item.getResources() << "," << item.getProfit() << "," << item.getTask() << endl;
-			}
-			cout << endl;
-			cout << "profitArray :" << endl;
-			problemSolver.printProfitArray();
-			cout << endl;
-		}
-	}
-	cout << "for serial execution : " << totalTimeForSerialResolution << "ms" << endl;
-	cout << "for parallel execution : " << totalTimeForParallelResolution << "ms" << endl;
-	cout << "for branch and bound execution : " << totalTimeForBBResolution << "ms" << endl;
 	
+	std::vector<long long> timesSerialDP;
+	std::vector<long long> timesparallelDP;
+	std::vector<long long> timesSerialBB;
+	vector<int> xAxis;
 
+	for (int numberOfitems = 1; numberOfitems < maxNumberOfItems; numberOfitems = numberOfitems*2) {
+		
+		long long totalTimeForSerialResolution = 0;
+		long long totalTimeForParallelResolution = 0;
+		long long totalTimeForBBResolution = 0;
+		
+		for (int testNumber = 0; testNumber < 100; testNumber++) {
+
+			int numberOfSet = dist3(rng);
+			int capacity = dist1(rng);
+			std::uniform_int_distribution<int> dist2(1, capacity);
+			unordered_map<int, JobConfiguration> mapItems;
+
+			for (int itemNumber = 0; itemNumber < numberOfitems; itemNumber++) {
+				std::generate(begin(v), end(v), [&]() { return dist2(rng); });
+				JobConfiguration newJobConfig = JobConfiguration(v[0], v[1], v[2] % numberOfSet);
+				mapItems[newJobConfig.getId()] = newJobConfig;
+			}
+
+			auto start = get_time();
+			ResourceManagerDP problemSolver = ResourceManagerDP(capacity, mapItems);
+			SackComposition solution = problemSolver.solveMckp();
+			auto finish = get_time();
+			auto duration =
+				std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
+			totalTimeForSerialResolution += duration.count();
+
+
+			auto parallelStart = get_time();
+
+			ResourceManagerDP parallelProblemSolver = ResourceManagerDP(capacity, mapItems);
+			SackComposition parallelSolution = parallelProblemSolver.solveMckpConcurrently();
+
+			auto parallelFinish = get_time();
+
+			auto parallelDuration =
+				std::chrono::duration_cast<std::chrono::nanoseconds>(parallelFinish - parallelStart);
+			totalTimeForParallelResolution += parallelDuration.count();
+
+			auto bBstart = get_time();
+			ResourceManagerBB problemSolverBB = ResourceManagerBB(capacity, mapItems);
+			SackComposition solutionBB = problemSolverBB.solveMckp();
+
+			auto bBFinish = get_time();
+
+			auto bBDuration =
+				std::chrono::duration_cast<std::chrono::nanoseconds>(bBFinish - bBstart);
+			totalTimeForBBResolution += bBDuration.count();
+
+			if (solutionBB.calculateTotalProfit() != solution.calculateTotalProfit()) {
+				std::cout << "There is a problem" << std::endl;
+				std::cout << solution.calculateTotalProfit() << endl;
+				std::cout << solutionBB.calculateTotalProfit() << endl;
+				std::cout << "knapsack :" << capacity << endl;
+				std::cout << "DP Solution : " << endl;
+				for (JobConfiguration item : solution.getItemList()) {
+					std::cout << item.getId() << "," << item.getResources() << "," << item.getProfit() << "," << item.getTask() << endl;
+				}
+				std::cout << endl;
+				std::cout << "BB Solution : " << endl;
+				for (JobConfiguration item : solutionBB.getItemList()) {
+					std::cout << item.getId() << "," << item.getResources() << "," << item.getProfit() << "," << item.getTask() << endl;
+				}
+				std::cout << "mapItems" << endl;
+				for (auto [key, item] : mapItems) {
+					std::cout << item.getId() << "," << item.getResources() << "," << item.getProfit() << "," << item.getTask() << endl;
+				}
+				std::cout << endl;
+				std::cout << "profitArray :" << endl;
+				problemSolver.printProfitArray();
+				std::cout << endl;
+			}
+		}
+		xAxis.push_back(numberOfitems);
+		timesSerialDP.push_back(totalTimeForSerialResolution);
+		timesparallelDP.push_back(totalTimeForParallelResolution);
+		timesSerialBB.push_back(totalTimeForBBResolution);
+		/*
+		std::cout << "for serial execution : " << totalTimeForSerialResolution << "ms" << endl;
+		std::cout << "for parallel execution : " << totalTimeForParallelResolution << "ms" << endl;
+		std::cout << "for branch and bound execution : " << totalTimeForBBResolution << "ms" << endl;
+		*/
+	}
+	for (auto value : xAxis) {
+		std::cout<< value <<endl;
+	}
+	std::cout << endl;
+	for (auto value : timesSerialDP) {
+		std::cout << value << endl;
+	}
+	std::cout << endl;
+	for (auto value : timesparallelDP) {
+		std::cout << value << endl;
+	}
+	std::cout << endl;
+	for (auto value : timesSerialBB) {
+		std::cout << value << endl;
+	}
+	std::cout << endl;
 	/*
 	unordered_map<int, JobConfiguration> mapItems;
 	mapItems[1] = JobConfiguration(4, 4, 1);
